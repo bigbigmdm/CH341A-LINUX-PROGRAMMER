779ee8e8483c10c7ff6d4240a1c994c8906b47f6
Mikhail Medvedev <75389103+bigbigmdm@users.noreply.github.com>
2021-04-30 14:55:42 +0300

    Hex editor QT

---
+++ example/mainwindow.h
@@ -0,0 +1,112 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+#include "../src/qhexedit.h"
+#include "optionsdialog.h"
+#include "searchdialog.h"
+#include "eepromsize.h"
+
+QT_BEGIN_NAMESPACE
+class QAction;
+class QMenu;
+class QUndoStack;
+class QLabel;
+class QDragEnterEvent;
+class QDropEvent;
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow();
+
+protected:
+    void closeEvent(QCloseEvent *event);
+    void dragEnterEvent(QDragEnterEvent *event);
+    void dropEvent(QDropEvent *event);
+
+private slots:
+    void about();
+    void dataChanged();
+    void open();
+    void optionsAccepted();
+    void findNext();
+    bool save();
+    bool saveAs();
+    void saveSelectionToReadableFile();
+    void saveToReadableFile();
+    void setAddress(qint64 address);
+    void setOverwriteMode(bool mode);
+    void setSize(qint64 size);
+    void showOptionsDialog();
+    void showSearchDialog();
+//
+    void showEepromSize();
+    void readActBt();
+    void writeActBt();
+    void selectActBt();
+    void setChipType(QString ch_type);
+
+//
+private:
+    void init();
+    void createActions();
+    void createMenus();
+    void createStatusBar();
+    void createToolBars();
+    void loadFile(const QString &fileName);
+    void readSettings();
+    bool saveFile(const QString &fileName);
+    void setCurrentFile(const QString &fileName);
+    QString strippedName(const QString &fullFileName);
+    void writeSettings();
+
+    QString CH_TYPE;
+    QString curFile;
+    QFile file;
+    bool isUntitled;
+
+    QMenu *fileMenu;
+    QMenu *editMenu;
+    QMenu *helpMenu;
+
+    QToolBar *fileToolBar;
+    QToolBar *editToolBar;
+
+    QAction *openAct;
+//
+    QAction *readAct;
+    QAction *writeAct;
+    QAction *selectAct;
+    QAction *saveAct;
+    QAction *saveAsAct;
+    QAction *saveReadable;
+    QAction *closeAct;
+    QAction *exitAct;
+
+    QAction *undoAct;
+    QAction *redoAct;
+    QAction *saveSelectionReadable;
+
+    QAction *aboutAct;
+    QAction *aboutQtAct;
+    QAction *optionsAct;
+    QAction *findAct;
+    QAction *findNextAct;
+
+    QHexEdit *hexEdit;
+    OptionsDialog *optionsDialog;
+    SearchDialog *searchDialog;
+//
+    EepromSize *eepromSize;
+    QLabel *lbChipType, *lbChipTypeName;
+    QLabel *lbAddress, *lbAddressName;
+    QLabel *lbOverwriteMode, *lbOverwriteModeName;
+    QLabel *lbSize, *lbSizeName;
+};
+
+#endif
